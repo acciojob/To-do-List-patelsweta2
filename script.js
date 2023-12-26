b27a2d4db5b43c133e6c72ba63c48834998307a5
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2023-12-26 19:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here
+const newTodoIn = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
 
+addTodoBtn.addEventListener("click", function(){
+    let todoText = newTodoIn.value;
+
+    if(todoText.trim() !== ""){
+        let listItem = document.createElement("li");
+        listItem.textContent = todoText;
+
+        todoList.appendChild(listItem);
+        newTodoIn.value = "";  // Fix the property name
+    }
+});
